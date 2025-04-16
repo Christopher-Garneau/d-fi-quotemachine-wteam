c9d7b9293b8befe13bedaa46262c5539e7693bfc
houssem Eddine <houssem_eddine94@hotmail.com>
2025-04-16 08:45:16 -0400

    Avancement de la methode Load avec validation

--- QuoteMachine-ExerciceGit/QuoteManager.cs
+++ QuoteMachine-ExerciceGit/QuoteManager.cs
@@ -48,17 +48,75 @@ namespace QuoteMachine_ExerciceGit
             throw new NotImplementedException("À implémenter dans feature/save-to-file");
         }
 
+        //public void LoadFromCSVFile(string path)
+        //{
+        //    //Avant de commencer, décommenter les tests suivants:
+        //    //LoadFromFile_ShouldAppendQuotesToList
+        //    //LoadFromFile_ShouldThrowIfFileMissing
+        //    //LoadFromFile_ShouldThrowIfNotInCSVExtension
+
+        //    //Avant de créer votre PR, faites un git rebase sur main pour vous assurer que vous avez la dernière version du code.
+
+        //    throw new NotImplementedException("À implémenter dans feature/load-from-file");
+        //}
+
+
+
         public void LoadFromCSVFile(string path)
         {
-            //Avant de commencer, décommenter les tests suivants:
-            //LoadFromFile_ShouldAppendQuotesToList
-            //LoadFromFile_ShouldThrowIfFileMissing
-            //LoadFromFile_ShouldThrowIfNotInCSVExtension
+            if (!IsCSVFile(path))
+            {
+                throw new QuoteFileException("Erreur lors de la sauvegarde : le fichier doit avoir l'extension .csv");
+            }
 
-            //Avant de créer votre PR, faites un git rebase sur main pour vous assurer que vous avez la dernière version du code.
+            if (!File.Exists(path))
+            {
+                throw new QuoteFileException("Erreur lors du chargement : le fichier n'existe pas");
+            }
 
-            throw new NotImplementedException("À implémenter dans feature/load-from-file");
+            try
+            {
+                var lignes = File.ReadAllLines(path);
+                foreach (var ligne in lignes)
+                {
+                    var parties = ligne.Split(',');
+                    if (parties.Length == 2)
+                    {
+                        _quotes.Add(new Quote
+                        {
+                            Text = parties[0].Trim(),
+                            Author = parties[1].Trim()
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new QuoteFileException("Erreur lors du chargement du fichier.", ex);
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public List<Quote> GetAllQuotes()
         {
